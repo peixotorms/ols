07cd3e91b47fd04afec86e477a4d4abb1aa73cd0
Raul Peixoto <raul.peixoto@gmail.com>
2023-04-06 22:24:58 +0300

    Update vhost.sh

--- vhost.sh
+++ vhost.sh
@@ -34,15 +34,6 @@ generate_user_name() {
 }
 
 
-# Initialize default values
-ssl="yes"
-php="8.0"
-db_host="localhost"
-db_port="3306"
-wp_install="yes"
-dev_mode="yes"
-aliases=()
-
 TEMP=$(getopt -o '' --long domain:,aliases:,ssl:,php:,path:,sftp_user:,sftp_pass:,db_host:,db_port:,db_user:,db_pass:,wp_install:,wp_user:,wp_pass:,dev_mode: -n "$(basename -- "$0")" -- "$@")
 eval set -- "$TEMP"
 
@@ -52,7 +43,7 @@ while true; do
         --domain)
             domain="${2}"; shift 2
             if ! validate_domain "$domain"; then
-                echo "Invalid domain name"; exit 1
+                print_colored red "Invalid domain name $domain"; exit 1
             fi
 			
 			# Create new variable without www subdomain
@@ -69,12 +60,21 @@ while true; do
             db_pass="$(gen_rand_pass)"
             wp_pass="$(gen_rand_pass)"
 			
+			# Initialize default values
+			ssl="yes"
+			php="8.0"
+			db_host="localhost"
+			db_port="3306"
+			wp_install="yes"
+			dev_mode="yes"
+			aliases=()
+			
             ;;
         --aliases)
             IFS=',' read -ra aliases <<< "${2:-}"; shift 2
             for alias in "${aliases[@]}"; do
                 if ! validate_domain "$alias"; then
-                    echo "Invalid alias domain: $alias"; exit 1
+                    print_colored red "Invalid alias domain: $alias"; exit 1
                 fi
             done
             ;;
@@ -84,7 +84,7 @@ while true; do
                     ssl="${2,,}"; shift 2
                     ;;
                 *)
-                    echo "Invalid SSL value: $2. Must be 'yes' or 'no'."; exit 1
+                    print_colored red "Invalid SSL value: $2. Must be 'yes' or 'no'."; exit 1
                     ;;
             esac
             ;;
@@ -94,14 +94,14 @@ while true; do
                     php="${2,,}"; shift 2
                     ;;
                 *)
-                    echo "Invalid PHP version: $2. Must be 7.4, 8.0, 8.1, or 8.2."; exit 1
+                    print_colored red "Invalid PHP version: $2. Must be 7.4, 8.0, 8.1, or 8.2."; exit 1
                     ;;
             esac
             ;;
         --path)
             path="${2:-/home/sites/$domain_no_www}"; shift 2
             if [[ "${path%/}" != *"/$domain_no_www" ]]; then
-                echo "Invalid path: must include domain as the last directory name"; exit 1
+                print_colored red "Invalid path: must include domain as the last directory name"; exit 1
             fi
             ;;
         --sftp_user)
@@ -113,9 +113,9 @@ while true; do
         --sftp_pass)
 			sftp_pass="${2:-$(gen_rand_pass)}"
 			if [[ "$sftp_pass" =~ [^a-zA-Z0-9,+=@\-_!] ]]; then
-				echo "Invalid SFTP password format. Only alphanumeric characters and these special characters are allowed: ,+=@-_!"; exit 1
+				print_colored red "Invalid SFTP password format. Only alphanumeric characters and these special characters are allowed: ,+=@-_!"; exit 1
 			elif [[ "${#sftp_pass}" -lt 8 ]] || [[ "${#sftp_pass}" -gt 32 ]]; then
-				echo "Invalid SFTP password length. Must be between 8 and 32 characters."; exit 1
+				print_colored red "Invalid SFTP password length. Must be between 8 and 32 characters."; exit 1
 			fi
 			shift 2
 			;;
@@ -136,9 +136,9 @@ while true; do
         --db_pass)
             db_pass="${2:-$(gen_rand_pass)}"
             if [[ "$db_pass" =~ [^a-zA-Z0-9,+=@\-_!] ]]; then
-                echo "Invalid database password format. Only alphanumeric characters and these special characters are allowed: ,+=@-_!"; exit 1
+                print_colored red "Invalid database password format. Only alphanumeric characters and these special characters are allowed: ,+=@-_!"; exit 1
             elif [[ "${#db_pass}" -lt 8 ]] || [[ "${#db_pass}" -gt 32 ]]; then
-                echo "Invalid database password length. Must be between 8 and 32 characters."; exit 1
+                print_colored red "Invalid database password length. Must be between 8 and 32 characters."; exit 1
             fi
             shift 2
             ;;
@@ -148,7 +148,7 @@ while true; do
                     wp_install="${2,,}"; shift 2
                     ;;
                 *)
-                    echo "Invalid WordPress installation value: $2. Must be 'yes' or 'no'."; exit 1
+                    print_colored red "Invalid WordPress installation value: $2. Must be 'yes' or 'no'."; exit 1
                     ;;
             esac
             ;;
@@ -163,27 +163,27 @@ while true; do
         --wp_pass)
             wp_pass="${2:-$(gen_rand_pass)}"
             if [[ "$wp_pass" =~ [^a-zA-Z0-9,+=@\-_!] ]]; then
-                echo "Invalid WordPress password format. Only alphanumeric characters and these special characters are allowed: ,+=@-_!"; exit 1
+                print_colored red "Invalid WordPress password format. Only alphanumeric characters and these special characters are allowed: ,+=@-_!"; exit 1
             elif [[ "${#wp_pass}" -lt 8 ]] || [[ "${#wp_pass}" -gt 32 ]]; then
-                echo "Invalid WordPress password length. Must be between 8 and 32 characters."; exit 1
+                print_colored red "Invalid WordPress password length. Must be between 8 and 32 characters."; exit 1
             fi
             shift 2
             ;;
         --dev_mode)
-            case "${2,,}" in
-                yes|no)
-                    dev_mode="${2,,}"; shift 2
-                    ;;
-                *)
-                    echo "Invalid dev mode value: $2. Must be 'yes' or 'no'."; exit 1
-            fi
-            ;;
+			case "${2,,}" in
+				yes|no)
+					dev_mode="${2,,}"; shift 2
+					;;
+				*)
+					print_colored red "Invalid dev mode value: $2. Must be 'yes' or 'no'."; exit 1
+			esac
+			;;
         --)
             shift
             break
             ;;
         *)
-            echo "Internal error!"; exit 1
+            print_colored red "Internal error!"; exit 1
             ;;
     esac
 	
@@ -195,25 +195,26 @@ done
 
 # Ensure --domain is mandatory
 if [[ -z "$domain" ]]; then
-    echo "Error: --domain option is required."; exit 1
+    print_colored red "Error: --domain option is required."; exit 1
 fi
 
 
 # Add your script logic below to process the options
 
 
-echo "Domain: $domain"
-echo "Aliases: ${alias_list[*]}"
-echo "SSL: $ssl"
-echo "PHP version: $php"
-echo "Path: $path"
-echo "SFTP user: $sftp_user"
-echo "SFTP password: $sftp_pass"
-echo "DB host: $db_host"
-echo "DB port: $db_port"
-echo "DB user: $db_user"
-echo "DB password: $db_pass"
-echo "WP install: $wp_install"
-echo "WP user: $wp_user"
-echo "WP password: $wp_pass"
-echo "Dev mode: $dev_mode"
+printf "%-15s %s\n" "Domain:" "$domain"
+printf "%-15s %s\n" "Aliases:" "${alias_list[*]}"
+printf "%-15s %s\n" "SSL:" "$ssl"
+printf "%-15s %s\n" "PHP version:" "$php"
+printf "%-15s %s\n" "Path:" "$path"
+printf "%-15s %s\n" "SFTP user:" "$sftp_user"
+printf "%-15s %s\n" "SFTP password:" "$sftp_pass"
+printf "%-15s %s\n" "DB host:" "$db_host"
+printf "%-15s %s\n" "DB port:" "$db_port"
+printf "%-15s %s\n" "DB user:" "$db_user"
+printf "%-15s %s\n" "DB password:" "$db_pass"
+printf "%-15s %s\n" "WP install:" "$wp_install"
+printf "%-15s %s\n" "WP user:" "$wp_user"
+printf "%-15s %s\n" "WP password:" "$wp_pass"
+printf "%-15s %s\n" "Dev mode:" "$dev_mode"
+
