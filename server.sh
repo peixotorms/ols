38a940704d407738110c13da738bb8eafb66a1ce
Raul Peixoto <raul.peixoto@gmail.com>
2023-05-04 00:51:41 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -215,7 +215,7 @@ function setup_repositories
 {
 	# percona
 	echo "Adding Percona repositories..."
-	silent curl -sO https://repo.percona.com/apt/percona-release_latest.generic_all.deb -o /tmp/percona-release_latest.generic_all.deb
+	silent curl -s https://repo.percona.com/apt/percona-release_latest.generic_all.deb -o /tmp/percona-release_latest.generic_all.deb
 	silent apt-get -y -f install gnupg2 lsb-release /tmp/percona-release_latest.generic_all.deb
 	rm /tmp/percona-release_latest.generic_all.deb
 	
